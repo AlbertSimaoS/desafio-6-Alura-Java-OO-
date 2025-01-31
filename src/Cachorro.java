8c84e2e0c23d0030a6ee03b4a4b5fb7867d2b4f0
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-01-31 15:21:32 -0300

    Update Cachorro.java

--- src/Cachorro.java
+++ src/Cachorro.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+public class Cachorro extends Animal{
+    @Override
+    public void emitirSom() {
+        System.out.println("Au, au");
+    }
 
-public class Cachorro {
+    public void abanarRabo(){
+        System.out.println("Cachorro abanando o rabo");
+    }
 }
