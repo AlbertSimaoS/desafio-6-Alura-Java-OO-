452cb554f3d55073f4ba6489904a63882391680c
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-01-31 15:21:14 -0300

    Update Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,11 +1,5 @@
-public class Main {
-    public static void main(String[] args) {
-        Cachorro cachorro = new Cachorro();
-        cachorro.emitirSom();
-        cachorro.abanarRabo();
-
-        Gato gato = new Gato();
-        gato.emitirSom();
-        gato.arranharMoveis();
+public class Animal {
+    public void emitirSom(){
+        System.out.println("Som do Animal");
     }
 }
